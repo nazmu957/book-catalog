f15da324486815cfba5088b7b995664fe13d5ab6
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-09-08 15:40:07 +0600

    products api get data from products api

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,14 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://book-catalog-server-navy-theta.vercel.app' }),
-  endpoints: (builder) => ({
-    getProducts: builder.query({
-      query: () => '/products',
-    }),
-    singleProduct: builder.query({
-      query: (id) => `/product/${id}`,
-    }),
-  }),
-});
+  endpoints: () => ({}),
 
-export const { useGetProductsQuery } = api;
+});
