0cbc01095dffdc5e9caa2c2bae8ff5e1646d2862
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-09-10 11:32:00 +0600

    new product added feature created succesfully

--- src/pages/AddBook.tsx
+++ src/pages/AddBook.tsx
@@ -1,4 +1,5 @@
 import { Textarea } from '@/components/ui/textarea';
+import { usePostProductMutation } from '@/redux/api/apiSlice';
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { FiSend } from 'react-icons/fi';
 
@@ -10,6 +11,15 @@ export default function AddBook() {
   const [inputValue1, setInputValue1] = useState<string>('');
   const [inputValue2, setInputValue2] = useState<string>('');
   const [inputValue3, setInputValue3] = useState<string>('');
+  const [inputValue4, setInputValue4] = useState<string>('');
+  const [inputValue5, setInputValue5] = useState<string>('');
+
+  const [postProduct, { isLoading, isError, isSuccess }] =
+    usePostProductMutation();
+
+  console.log(isLoading);
+  console.log(isError);
+  console.log(isSuccess);
 
   const handleChange1 = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue1(event.target.value);
@@ -22,12 +32,31 @@ export default function AddBook() {
   const handleChange3 = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue3(event.target.value);
   };
+  const handleChange4 = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue4(event.target.value);
+  };
+  const handleChange5 = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue5(event.target.value);
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('Textarea 1:', inputValue1);
-    console.log('Textarea 2:', inputValue2);
-    console.log('Textarea 3:', inputValue3);
+    const newProduct = {
+      inputValue1,
+      inputValue2,
+      inputValue3,
+      inputValue4,
+    };
+    const option = {
+      data: {
+        title: inputValue1,
+        author: inputValue2,
+        genre: inputValue3,
+        image: inputValue4,
+        publicationdate: inputValue5,
+      },
+    };
+    postProduct(option);
   };
 
   return (
@@ -37,16 +66,31 @@ export default function AddBook() {
           className="min-h-[30px]"
           onChange={handleChange1}
           value={inputValue1}
+          placeholder="title"
         />
         <Textarea
           className="min-h-[30px]"
           onChange={handleChange2}
           value={inputValue2}
+          placeholder='author'
         />
         <Textarea
           className="min-h-[30px]"
           onChange={handleChange3}
           value={inputValue3}
+          placeholder='genre'
+        />
+        <Textarea
+          className="min-h-[30px]"
+          onChange={handleChange4}
+          value={inputValue4}
+          placeholder='image link'
+        />
+        <Textarea
+          className="min-h-[30px]"
+          onChange={handleChange5}
+          value={inputValue5}
+          placeholder='publication date'
         />
         <button
           type="submit"
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://book-catalog-server-navy-theta.vercel.app' }),
-  endpoints: () => ({}),
-
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://book-catalog-server-navy-theta.vercel.app',
+  }),
+  endpoints: (builder) => ({
+    postProduct: builder.mutation({
+      query: ({ data }) => ({
+        url: `/product`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
 });
+
+
+export const { usePostProductMutation } = api;
