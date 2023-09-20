45ca59996f2407c9aa11760ad636b326d134e848
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-09-20 23:28:23 +0600

    site ready for deploy

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -7,7 +7,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useForm } from 'react-hook-form';
-import { FcGoogle } from 'react-icons/fc';
 import { createUser } from '@/redux/features/user/userSlice';
 import { useAppDispatch } from '@/redux/hook';
 
